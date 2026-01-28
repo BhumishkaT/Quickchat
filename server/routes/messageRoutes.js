295a9f4d78abb7cd162a9de32b57556592ba37a0
BhumishkaT <156921851+BhumishkaT@users.noreply.github.com>
2026-01-28 20:06:36 +0530

    Update messageRoutes.js

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.js';
-import { getMessages, getUserForSideBar, markMessageSeen, sendMessage } from '../controllers/messageControllers.js';
+import { getMessages, getUserForSideBar, markMessageSeen, sendMessage } from '../controllers/messageController.js';
 
 const messageRouter = express.Router();
 
